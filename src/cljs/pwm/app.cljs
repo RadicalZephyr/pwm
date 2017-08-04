f6a22539818cae561a7578d669769f198cfa5e60
Geoff Shannon <geoffpshannon@gmail.com>
2017-08-04 02:29:44 -0600

    Display a grid of different frequencies/duty cycles

--- src/cljs/pwm/app.cljs
+++ src/cljs/pwm/app.cljs
@@ -3,11 +3,17 @@
 
 (defn led [& classes]
   [:span.blink {:class classes
-          :dangerouslySetInnerHTML {:__html "&nbsp;"}}])
+                :dangerouslySetInnerHTML {:__html "&nbsp;"}}])
 
 (defn root []
   [:div
-   [led :f1w50]])
+   (for [freq ["1-8" "1-4" "1-2" "1" "2" "4"]]
+     ^{:key freq}
+     [:div
+      (for [width (range 10 91 10)]
+        ^{:key (str freq "-" width)}
+        [led (str "f" freq "w" width)])])])
+
 (defn init []
   (reagent/render-component [root]
                             (.getElementById js/document "container")))
