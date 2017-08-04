6df4738297c94585e1e8a73a06eb73472526a82a
Geoff Shannon <geoffpshannon@gmail.com>
2017-08-04 02:14:38 -0600

    Generate lots of animations and style rules

--- src/clj/pwm/styles.clj
+++ src/clj/pwm/styles.clj
@@ -1,21 +1,37 @@
 (ns pwm.styles
   (:require [garden.def :refer [defrule defstyles]]
             [garden.stylesheet :refer [rule at-keyframes]]
-            [garden.selectors :as sel]))
+            [garden.selectors :as sel]
+            [clojure.string :as str]))
+
+(def frequencies
+  (sort
+   (concat
+    (for [num (range 1 10)
+          denom (range 2 11)
+          :when (> denom num)]
+      (/ num denom))
+    (range 1 11))))
+
+(def widths (range 1 100))
 
 (defstyles screen
-  [:.blink
+  [:span.blink
    {:display "inline-block"
-    :width "1em"}]
+    :width "1em"
+    :margin "1em"}]
 
-  [:.f1w50
-   {:animation-duration "1s"
-    :animation-name "blink50"
-    :animation-iteration-count "infinite"}]
+  (for [f frequencies
+        w widths]
+    [(str/replace (format ".f%sw%s" f w) #"/" "-")
+     {:animation-duration (str (float f) "s")
+      :animation-name (str "blink" w)
+      :animation-iteration-count "infinite"}])
 
-  (at-keyframes "blink50"
-    [:from {:background-color "white"}]
-    ["49%" {:background-color "white"}]
-    ["50%" {:background-color "black"}]
-    ["99%" {:background-color "black"}]
-    [:to {:background-color "white"}]))
+  (for [w widths]
+    (at-keyframes (str "blink" w)
+      [:from {:background-color "white"}]
+      [(str (dec w) "%") {:background-color "white"}]
+      [(str w "%") {:background-color "black"}]
+      ["99%" {:background-color "black"}]
+      [:to {:background-color "white"}])))
